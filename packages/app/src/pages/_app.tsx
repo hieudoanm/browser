320cc17a733a39b5099678196757b7c9c9884886
HIEU DOAN <49260251+hieudoanm@users.noreply.github.com>
2026-03-25 21:27:08 +0700

    Use HeadTemplate

--- packages/app/src/pages/_app.tsx
+++ packages/app/src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import '@browser/styles/globals.css';
+import { HeadTemplate } from '../templates/HeadTemplate';
 import type { AppProps } from 'next/app';
 import { Geist, Geist_Mono } from 'next/font/google';
-import Head from 'next/head';
 import { FC } from 'react';
 
 const geistSans = Geist({
@@ -17,15 +17,9 @@ const geistMono = Geist_Mono({
 const App: FC<AppProps> = ({ Component, pageProps }) => {
   return (
     <>
-      <Head>
-        <title>Browser SDK</title>
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="manifest" href="/manifest.json" />
-        {/* Meta tags for SEO and social media */}
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="description" content="browser/sdk" />
-      </Head>
+      <HeadTemplate
+        basic={{ title: 'Browser SDK', description: 'browser/sdk' }}
+      />
       <div className={`${geistSans.className} ${geistMono.className}`}>
         <Component {...pageProps} />
       </div>
